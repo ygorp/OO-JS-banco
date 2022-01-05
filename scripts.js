7362bf0ef528a54eabd7ed8ca62cae1079998932
Ygor Pacheco Nogueira <ygor-nogueira@hotmail.com>
2022-01-05 12:24:59 -0300

    ...

--- scripts.js
+++ scripts.js
@@ -1,9 +1,9 @@
 class ContaBancaria {
-  constructor(agencia, numero, tipo, saldo) {
+  constructor(agencia, numero, tipo) {
     this.agencia = agencia;
     this.numero = numero;
     this.tipo = tipo;
-    this._saldo = saldo;
+    this._saldo = 0;
   }
 
   get saldo(){
@@ -30,9 +30,9 @@ class ContaBancaria {
   }
 }
 
-class ContaCorrente {
-  constructor(agencia, numero, saldo, cartaoCredito) {
-    super(agencia, numero, saldo);
+class ContaCorrente extends ContaBancaria {
+  constructor(agencia, numero, cartaoCredito) {
+    super(agencia, numero);
     this.tipo = 'corrente';
     this.cartaoCredito = cartaoCredito;
   }
@@ -46,16 +46,16 @@ class ContaCorrente {
   }
 }
 
-class ContaPoupanca {
-  constructor(agencia, numero, saldo) {
-    super(agencia, numero, saldo);
+class ContaPoupanca extends ContaBancaria {
+  constructor(agencia, numero) {
+    super(agencia, numero);
     this.tipo = 'poupança';
   }
 }
 
-class ContaUniversitaria {
-  constructor(agencia, numero, saldo) {
-    super(agencia, numero, saldo);
+class ContaUniversitaria extends ContaBancaria {
+  constructor(agencia, numero) {
+    super(agencia, numero);
     this.tipo = 'universitária';
   }
 
